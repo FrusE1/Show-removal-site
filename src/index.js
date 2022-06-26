c898b1bed39d7c8166b667d72541f11041968f43
Marsel <fathutdinov.2000@mail.ru>
2022-06-26 10:26:17 +0300

    bootstrap was connected

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
+import 'bootstrap';
+import * as bootstrap from 'bootstrap';
+
 const importAllFiles = resolve => resolve.keys().forEach(resolve);
 
 importAllFiles(require.context('../src/', true, /\.js$|\.scss$/));
